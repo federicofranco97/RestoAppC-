290daeb5d11b928c5ed5d7b0ea99e1cca3a034c8
federico franco- note <43964230+federicofranco97@users.noreply.github.com>
2019-06-09 19:46:45 -0300

    created app

---
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,313 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtMesero = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtMesa = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtSubtotal = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.mesa12 = new RestoApp.RoundButton();
+            this.mesa8 = new RestoApp.RoundButton();
+            this.mesa11 = new RestoApp.RoundButton();
+            this.mesa7 = new RestoApp.RoundButton();
+            this.mesa10 = new RestoApp.RoundButton();
+            this.mesa6 = new RestoApp.RoundButton();
+            this.mesa9 = new RestoApp.RoundButton();
+            this.mesa5 = new RestoApp.RoundButton();
+            this.mesa4 = new RestoApp.RoundButton();
+            this.mesa3 = new RestoApp.RoundButton();
+            this.mesa2 = new RestoApp.RoundButton();
+            this.mesa1 = new RestoApp.RoundButton();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(731, 312);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(138, 40);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Cerrar Mesa";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtMesero
+            // 
+            this.txtMesero.Enabled = false;
+            this.txtMesero.Location = new System.Drawing.Point(728, 86);
+            this.txtMesero.Name = "txtMesero";
+            this.txtMesero.Size = new System.Drawing.Size(145, 20);
+            this.txtMesero.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(728, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Nombre Mesero";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(728, 120);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Numero Mesa";
+            // 
+            // txtMesa
+            // 
+            this.txtMesa.Enabled = false;
+            this.txtMesa.Location = new System.Drawing.Point(728, 139);
+            this.txtMesa.Name = "txtMesa";
+            this.txtMesa.Size = new System.Drawing.Size(145, 20);
+            this.txtMesa.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(728, 180);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(83, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Subtotal Cuenta";
+            // 
+            // txtSubtotal
+            // 
+            this.txtSubtotal.Enabled = false;
+            this.txtSubtotal.Location = new System.Drawing.Point(728, 199);
+            this.txtSubtotal.Name = "txtSubtotal";
+            this.txtSubtotal.Size = new System.Drawing.Size(145, 20);
+            this.txtSubtotal.TabIndex = 6;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(731, 258);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(138, 44);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Agregar Valor";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(731, 359);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(138, 38);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Tomar Mesa";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // mesa12
+            // 
+            this.mesa12.Location = new System.Drawing.Point(505, 296);
+            this.mesa12.Name = "mesa12";
+            this.mesa12.Size = new System.Drawing.Size(126, 113);
+            this.mesa12.TabIndex = 1;
+            this.mesa12.Text = "Mesa 12";
+            this.mesa12.UseVisualStyleBackColor = true;
+            this.mesa12.Click += new System.EventHandler(this.mesa12_Click);
+            // 
+            // mesa8
+            // 
+            this.mesa8.Location = new System.Drawing.Point(505, 167);
+            this.mesa8.Name = "mesa8";
+            this.mesa8.Size = new System.Drawing.Size(126, 113);
+            this.mesa8.TabIndex = 1;
+            this.mesa8.Text = "Mesa 8";
+            this.mesa8.UseVisualStyleBackColor = true;
+            this.mesa8.Click += new System.EventHandler(this.mesa8_Click);
+            // 
+            // mesa11
+            // 
+            this.mesa11.Location = new System.Drawing.Point(364, 296);
+            this.mesa11.Name = "mesa11";
+            this.mesa11.Size = new System.Drawing.Size(126, 113);
+            this.mesa11.TabIndex = 1;
+            this.mesa11.Text = "Mesa 11";
+            this.mesa11.UseVisualStyleBackColor = true;
+            this.mesa11.Click += new System.EventHandler(this.mesa11_Click);
+            // 
+            // mesa7
+            // 
+            this.mesa7.Location = new System.Drawing.Point(364, 167);
+            this.mesa7.Name = "mesa7";
+            this.mesa7.Size = new System.Drawing.Size(126, 113);
+            this.mesa7.TabIndex = 1;
+            this.mesa7.Text = "Mesa 7";
+            this.mesa7.UseVisualStyleBackColor = true;
+            this.mesa7.Click += new System.EventHandler(this.mesa7_Click);
+            // 
+            // mesa10
+            // 
+            this.mesa10.Location = new System.Drawing.Point(210, 296);
+            this.mesa10.Name = "mesa10";
+            this.mesa10.Size = new System.Drawing.Size(126, 113);
+            this.mesa10.TabIndex = 1;
+            this.mesa10.Text = "Mesa 10";
+            this.mesa10.UseVisualStyleBackColor = true;
+            this.mesa10.Click += new System.EventHandler(this.mesa10_Click);
+            // 
+            // mesa6
+            // 
+            this.mesa6.Location = new System.Drawing.Point(210, 167);
+            this.mesa6.Name = "mesa6";
+            this.mesa6.Size = new System.Drawing.Size(126, 113);
+            this.mesa6.TabIndex = 1;
+            this.mesa6.Text = "Mesa 6";
+            this.mesa6.UseVisualStyleBackColor = true;
+            this.mesa6.Click += new System.EventHandler(this.mesa6_Click);
+            // 
+            // mesa9
+            // 
+            this.mesa9.Location = new System.Drawing.Point(47, 296);
+            this.mesa9.Name = "mesa9";
+            this.mesa9.Size = new System.Drawing.Size(126, 113);
+            this.mesa9.TabIndex = 1;
+            this.mesa9.Text = "Mesa 9";
+            this.mesa9.UseVisualStyleBackColor = true;
+            this.mesa9.Click += new System.EventHandler(this.mesa9_Click);
+            // 
+            // mesa5
+            // 
+            this.mesa5.Location = new System.Drawing.Point(47, 167);
+            this.mesa5.Name = "mesa5";
+            this.mesa5.Size = new System.Drawing.Size(126, 113);
+            this.mesa5.TabIndex = 1;
+            this.mesa5.Text = "Mesa 5";
+            this.mesa5.UseVisualStyleBackColor = true;
+            this.mesa5.Click += new System.EventHandler(this.mesa5_Click);
+            // 
+            // mesa4
+            // 
+            this.mesa4.Location = new System.Drawing.Point(505, 24);
+            this.mesa4.Name = "mesa4";
+            this.mesa4.Size = new System.Drawing.Size(126, 113);
+            this.mesa4.TabIndex = 1;
+            this.mesa4.Text = "Mesa 4";
+            this.mesa4.UseVisualStyleBackColor = true;
+            this.mesa4.Click += new System.EventHandler(this.mesa4_Click);
+            // 
+            // mesa3
+            // 
+            this.mesa3.Location = new System.Drawing.Point(364, 24);
+            this.mesa3.Name = "mesa3";
+            this.mesa3.Size = new System.Drawing.Size(126, 113);
+            this.mesa3.TabIndex = 1;
+            this.mesa3.Text = "Mesa 3";
+            this.mesa3.UseVisualStyleBackColor = true;
+            this.mesa3.Click += new System.EventHandler(this.mesa3_Click);
+            // 
+            // mesa2
+            // 
+            this.mesa2.Location = new System.Drawing.Point(210, 24);
+            this.mesa2.Name = "mesa2";
+            this.mesa2.Size = new System.Drawing.Size(126, 113);
+            this.mesa2.TabIndex = 1;
+            this.mesa2.Text = "Mesa 2";
+            this.mesa2.UseVisualStyleBackColor = true;
+            this.mesa2.Click += new System.EventHandler(this.mesa2_Click);
+            // 
+            // mesa1
+            // 
+            this.mesa1.Location = new System.Drawing.Point(47, 24);
+            this.mesa1.Name = "mesa1";
+            this.mesa1.Size = new System.Drawing.Size(126, 113);
+            this.mesa1.TabIndex = 1;
+            this.mesa1.Text = "Mesa 1";
+            this.mesa1.UseVisualStyleBackColor = true;
+            this.mesa1.Click += new System.EventHandler(this.mesa1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(934, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtSubtotal);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtMesa);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtMesero);
+            this.Controls.Add(this.mesa12);
+            this.Controls.Add(this.mesa8);
+            this.Controls.Add(this.mesa11);
+            this.Controls.Add(this.mesa7);
+            this.Controls.Add(this.mesa10);
+            this.Controls.Add(this.mesa6);
+            this.Controls.Add(this.mesa9);
+            this.Controls.Add(this.mesa5);
+            this.Controls.Add(this.mesa4);
+            this.Controls.Add(this.mesa3);
+            this.Controls.Add(this.mesa2);
+            this.Controls.Add(this.mesa1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Resto App";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private RestoApp.RoundButton mesa1;
+        private RestoApp.RoundButton mesa2;
+        private RestoApp.RoundButton mesa3;
+        private RestoApp.RoundButton mesa4;
+        private RestoApp.RoundButton mesa5;
+        private RestoApp.RoundButton mesa6;
+        private RestoApp.RoundButton mesa7;
+        private RestoApp.RoundButton mesa8;
+        private System.Windows.Forms.TextBox txtMesero;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtMesa;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtSubtotal;
+        private RestoApp.RoundButton mesa9;
+        private RestoApp.RoundButton mesa10;
+        private RestoApp.RoundButton mesa11;
+        private RestoApp.RoundButton mesa12;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,437 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Collections;
+using System.Globalization;
+using System.Collections.Generic;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        private List<Button> list = new List<Button>();
+        private string conStr = ConfigurationManager.ConnectionStrings["restoApp"].ToString();
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            CenterToScreen();
+            llenarLista();
+            marcarMesas();
+        }
+
+        public void llenarLista()
+        {
+            list.Add(mesa1);
+            list.Add(mesa2);
+            list.Add(mesa3);
+            list.Add(mesa4);
+            list.Add(mesa5);
+            list.Add(mesa6);
+            list.Add(mesa7);
+            list.Add(mesa8);
+            list.Add(mesa9);
+            list.Add(mesa10);
+            list.Add(mesa11);
+            list.Add(mesa12);
+            
+        }
+
+        public void marcarMesas()
+        {
+            foreach(Button btn in list)
+            {
+                checkEstado(btn);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int id;
+            double valor;
+            try
+            {
+                id= Convert.ToInt16(txtMesa.Text);
+                //CultureInfo culture = new CultureInfo("ar");
+                valor = double.Parse(Interaction.InputBox("Ingrese valor a sumar"), NumberStyles.AllowDecimalPoint, NumberFormatInfo.InvariantInfo);
+            }
+            catch(Exception a)
+            {
+                MessageBox.Show( "El valor ingresado no es valido", "Error!");
+                return;
+            }
+            sumarSubtotal(id,valor);
+        }
+
+        public void sumarSubtotal(int id,double valor)
+        {
+            string query = "Select subtotal From mesa where id=" + id;
+            double aux=0;
+            using (SqlConnection connection = new SqlConnection(conStr))
+            {
+                SqlCommand command = new SqlCommand(query, connection);
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+                try
+                {
+                    while (reader.Read())
+                    {
+                        aux =(double)(reader["subtotal"]);
+                        
+                    }
+                }
+                finally
+                {
+                    reader.Close();
+                }
+            }
+            
+            aux =aux+valor;
+            String aux4 = "Update mesa set subtotal=" + aux + " where id=" + id;
+            String query2 = aux4.Replace(",",".");
+            using (SqlConnection connection = new SqlConnection(conStr))
+            {
+                SqlCommand command = new SqlCommand(query2, connection);
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+                try
+                {
+                    while (reader.Read())
+                    {
+                        MessageBox.Show("El valor fue sumado","Exito!" );                       
+                    }
+                }
+                finally
+                {
+                    reader.Close();
+                }
+            }
+           
+            list[id-1].PerformClick();
+            
+        }
+
+        public void mostrarData(Button b)
+        {
+            String[] aux = b.Text.Split(' ');
+            String choice = aux[1];
+            cargarData(choice);
+        }
+
+        private void checkEstado(Button b)
+        {
+            String[] aux = b.Text.Split(' ');
+            String choice = aux[1];
+            int id;
+            try
+            {
+                id = Convert.ToInt16(choice);
+            }
+            catch (Exception e)
+            {
+                return;
+            }
+            string query = "Select mesero From mesa where id=" + id;
+            using (SqlConnection connection = new SqlConnection(conStr))
+            {
+                SqlCommand command = new SqlCommand(query, connection);
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+                try
+                {
+                    while (reader.Read())
+                    {
+                        String cont = (String)(reader["mesero"]);
+                        if(cont.Equals("No tiene"))
+                        {
+                             b.BackColor = Color.FromArgb(0, 255, 0);
+                        }
+                        else
+                        {
+                            b.BackColor = Color.FromArgb(255, 0, 0);
+                        }
+                    }
+                }
+                finally
+                {
+                    reader.Close();
+                }
+            }
+        }
+
+        private void cargarData(String choice)
+        {
+            
+            int id;
+            try
+            {
+                id = Convert.ToInt16(choice);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show( "El valor ingresado no es valido", "Error!");
+                return;
+            }
+            string query = "Select mesero,subtotal From mesa where id=" + id;
+            using (SqlConnection connection = new SqlConnection(conStr))
+            {
+                SqlCommand command = new SqlCommand(query, connection);
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+                try
+                {
+                    while (reader.Read())
+                    {
+                        String cont = (String)(reader["mesero"]+" "+(reader["subtotal"]));
+                        if (cont == null || cont.Equals("") || cont.Equals(" "))
+                        {
+                            MessageBox.Show("No existe la entrada!");
+                        }
+                        else
+                        {
+                            txtMesero.Text = (String)(reader["mesero"]);
+                            txtMesa.Text = (String)choice;
+                            txtSubtotal.Text =Convert.ToString(reader["subtotal"]);
+                        }
+                    }
+                }
+                finally
+                {
+                    reader.Close();
+                }
+            }
+
+        }
+
+        public void cerrarMesa()
+        {
+            int idMesa;
+            double subtotal = 0;
+            string mesero = "";
+            try
+            {
+                idMesa = Convert.ToInt16(txtMesa.Text);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Primero selecciona una mesa!", "Error!");
+                return;
+            }
+            Boolean disponible = false;
+            String query = "Select * From mesa where id=" + idMesa;
+            using (SqlConnection connection = new SqlConnection(conStr))
+            {
+                SqlCommand command = new SqlCommand(query, connection);
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+                try
+                {
+                    while (reader.Read())
+                    {
+                        String cont = (String)(reader["mesero"]);
+                        if (!cont.Equals("No tiene"))
+                        {
+                            disponible = true;
+                            subtotal = (double)(reader["subtotal"]);
+                            mesero = (string)(reader["mesero"]);
+                        }
+                        else
+                        {
+                            disponible = false;
+                        }
+                    }
+                }
+                finally
+                {
+                    reader.Close();
+                }
+            }
+
+            if (!disponible)
+            {
+                MessageBox.Show("Esta mesa no esta ocupada!", "Error!");
+                return;
+            }
+
+            MessageBox.Show("El total de la mesa es: " + subtotal+"\nEl mesero fue :"+mesero+"\nLa propina esperada es: "+subtotal*0.1,"Cuenta");
+            vaciarMesa(idMesa);
+            marcarMesas();
+            list[idMesa - 1].PerformClick();
+        }
+
+        public void vaciarMesa(int id)
+        {
+            String query = "update mesa set mesero='No tiene',subtotal=0 where id=" + id;
+            using (SqlConnection connection = new SqlConnection(conStr))
+            {
+                SqlCommand command = new SqlCommand(query, connection);
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+                try
+                {
+
+                }
+                finally
+                {
+                    reader.Close();
+                }
+            }
+        }
+
+        public void tomarMesa()
+        {
+            int idMesa;
+            try
+            {
+                idMesa = Convert.ToInt16(txtMesa.Text);
+            }catch(Exception e)
+            {
+                MessageBox.Show( "Primero selecciona una mesa!", "Error!");
+                return;
+            }
+            Boolean disponible = false ;
+            String query = "Select mesero From mesa where id=" + idMesa;
+            using (SqlConnection connection = new SqlConnection(conStr))
+            {
+                SqlCommand command = new SqlCommand(query, connection);
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+                try
+                {
+                    while (reader.Read())
+                    {
+                        String cont = (String)(reader["mesero"]);
+                        if (cont.Equals("No tiene"))
+                        {
+                            disponible = true;
+                        }
+                        else
+                        {
+                            disponible = false;
+                        }
+                    }
+                }
+                finally
+                {
+                    reader.Close();
+                }
+            }
+
+            if (!disponible)
+            {
+                MessageBox.Show( "Esta mesa esta ocupada!", "Error!");
+            }
+            else
+            {
+                String choice = Interaction.InputBox("Ingrese el nombre del mesero");
+                if(choice != null && choice !="" && choice !=" ")
+                {
+                    String query2 = "UPDATE mesa SET mesero = '"+choice +"' WHERE id=" + idMesa;
+                    using (SqlConnection connection = new SqlConnection(conStr))
+                    {
+                        SqlCommand command = new SqlCommand(query2, connection);
+                        connection.Open();
+                        SqlDataReader reader = command.ExecuteReader();
+                        try
+                        {
+                            while (reader.Read())
+                            {
+                                String cont = (String)(reader["mesero"]);
+                                if (cont.Equals(choice))
+                                {
+                                    MessageBox.Show( "Mesa Actualizada","Exito!");
+                                    
+                                }
+                                else
+                                {
+                                    MessageBox.Show("Ha ocurrido un error!", "Error!");
+                                }
+                            }
+                        }
+                        finally
+                        {
+                            reader.Close();
+                        }
+                    }
+                }
+            }
+            marcarMesas();
+            list[idMesa - 1].PerformClick();
+        }
+
+        private void mesa1_Click(object sender, EventArgs e)
+        {
+            mostrarData(mesa1);
+        }
+
+        private void mesa2_Click(object sender, EventArgs e)
+        {
+            mostrarData(mesa2);
+        }
+
+        private void mesa3_Click(object sender, EventArgs e)
+        {
+            mostrarData(mesa3);
+        }
+
+        private void mesa4_Click(object sender, EventArgs e)
+        {
+            mostrarData(mesa4);
+        }
+
+        private void mesa5_Click(object sender, EventArgs e)
+        {
+            mostrarData(mesa5);
+        }
+
+        private void mesa6_Click(object sender, EventArgs e)
+        {
+            mostrarData(mesa6);
+        }
+
+        private void mesa7_Click(object sender, EventArgs e)
+        {
+            mostrarData(mesa7);
+        }
+
+        private void mesa8_Click(object sender, EventArgs e)
+        {
+            mostrarData(mesa8);
+        }
+
+        private void mesa9_Click(object sender, EventArgs e)
+        {
+            mostrarData(mesa9);
+        }
+
+        private void mesa10_Click(object sender, EventArgs e)
+        {
+            mostrarData(mesa10);
+        }
+
+        private void mesa11_Click(object sender, EventArgs e)
+        {
+            mostrarData(mesa11);
+        }
+
+        private void mesa12_Click(object sender, EventArgs e)
+        {
+            mostrarData(mesa12);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            tomarMesa();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            cerrarMesa();
+        }
+    }
+}
+
